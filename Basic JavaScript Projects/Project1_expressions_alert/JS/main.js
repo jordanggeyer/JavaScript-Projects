4cf0490f619b80b74c6c430329f486007d12e420
Jordan <jordanggeyer@gmail.com>
2024-05-07 19:16:36 -0700

    Pushing “Project1_expressions_alert” folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var J = "My name is Jordan!";                                                   //Setting Variable "J" to "My name is Jordan!"
+window.alert(J);                                                                //Creating window upon opening that displays the value of "J"
+
+document.write("Why can\'t I beat this level?")                                 //Showing the "\" as a special use character
+document.write(" My Name" + " is Jordan" + "!")                                 //Showing concatenation
+
+var B = " N" + "B" + "A"                                                        //Showing concatenation assigned to a variable
+document.write(B)                                                               //Displaying the value of "B"
+
+var Last_Name = "Geyer", First_Name = " Jordan", Middle_Name = "Gabriel";       //Setting multiple variables at once
+document.write(First_Name)                                                      //Displaying the value for the variable "First_Name"
+
+document.write(3+3)                                                             //Displaying the use of an expression (3+3)
