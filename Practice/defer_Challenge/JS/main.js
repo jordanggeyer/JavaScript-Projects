ad8b2425a1f68d8fd038679083eb7375ce1e78bd
Jordan <jordanggeyer@gmail.com>
2024-05-19 13:34:31 -0700

    Completion of Project10_loops_arrays

---
+++ Practice/defer_Challenge/JS/main.js
@@ -0,0 +1,8 @@
+function deferred_function() {
+    var x = 75;
+    var formula = (x + 9);
+    document.getElementById("deferred").innerHTML = formula;
+    console.log(formula)
+}
+
+deferred_function();
