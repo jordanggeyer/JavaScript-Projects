53f7ae4720da2075a71d998d740ba5368d9368c1
Jordan <jordanggeyer@gmail.com>
2025-02-19 19:13:51 -0700

    Remove practice files to leave finished projects

--- Practice/defer_Challenge/JS/main.js
+++
@@ -1,8 +0,0 @@
-function deferred_function() {
-    var x = 75;
-    var formula = (x + 9);
-    document.getElementById("deferred").innerHTML = formula;
-    console.log(formula)
-}
-
-deferred_function();
